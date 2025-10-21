88a183d7d3f520e9bdbc05560dd6f7419ae38c76
gxstar <gengxing123@qq.com>
2025-10-21 12:03:09 +0800

    feat: 添加Qt界面支持并修改样式组件
    - 将控制台应用改为Qt界面应用，添加主窗口加载逻辑
    - 将单选按钮改为复选框并更新相关样式
    - 设置默认质量值为100并更新显示

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from image-convert-qt!")
+import sys
+from PySide6.QtWidgets import QApplication
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile, QIODevice
 
+app = QApplication(sys.argv)
 
-if __name__ == "__main__":
-    main()
+ui_file = QFile("main.ui")
+ui_file.open(QIODevice.ReadOnly)
+window = QUiLoader().load(ui_file)
+ui_file.close()
+
+window.show()
+sys.exit(app.exec())
